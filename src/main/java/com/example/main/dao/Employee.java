2688e4dfa9617f282371c96e922039ab4bb414fc
shreyaaaaaa <37182432+shreyaaaaaa@users.noreply.github.com>
2019-03-29 16:15:49 +0530

    removed java project

--- src/main/java/com/example/main/dao/Employee.java
+++
@@ -1,9 +0,0 @@
-package com.example.main.dao;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
-
-
-public interface Employee {
-	public String changeName(); 
-}
